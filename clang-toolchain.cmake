d431d76a9b4161a7d5cf253f6708df8e3bf76960
Morten V. Pedersen <morten@mortenvp.com>
2025-05-18 19:54:17 +0200

    Use default c++ standard library

--- clang-toolchain.cmake
+++ clang-toolchain.cmake
@@ -4,5 +4,3 @@
 set(CMAKE_C_COMPILER clang CACHE STRING "C compiler" FORCE)
 set(CMAKE_CXX_COMPILER clang++ CACHE STRING "C++ compiler" FORCE)
 
-# set standard lib to libc++
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -stdlib=libc++" CACHE STRING "C++ flags" FORCE)
