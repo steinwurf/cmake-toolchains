aaa12e26f96acaacff9b9f76090ec1caa9b806a3
Morten V. Pedersen <morten@mortenvp.com>
2025-05-18 16:17:05 +0200

    Adding GCC toolchain

---
+++ gcc-toolchain.cmake
@@ -0,0 +1,6 @@
+# gcc-toolchain.cmake
+
+# Set the C and C++ compilers to gcc
+set(CMAKE_C_COMPILER gcc CACHE STRING "C compiler" FORCE)
+set(CMAKE_CXX_COMPILER g++ CACHE STRING "C++ compiler" FORCE)
+
