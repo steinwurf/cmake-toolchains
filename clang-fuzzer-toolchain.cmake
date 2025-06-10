33e32e3522558bb347339d1cfa63c7eef3a0972a
Jeppe Berg Axlesen <jaxels20@student.aau.dk>
2025-06-10 07:41:27 +0200

    Add clang fuzzer toolchain

---
+++ clang-fuzzer-toolchain.cmake
@@ -0,0 +1,11 @@
+# Use Clang
+set(CMAKE_CXX_COMPILER clang++)
+set(CMAKE_C_COMPILER clang)
+
+# Use LLVM's lld linker
+set(CMAKE_EXE_LINKER_FLAGS "-fuse-ld=lld" CACHE STRING "Use lld as the linker" FORCE)
+set(CMAKE_SHARED_LINKER_FLAGS "-fuse-ld=lld" CACHE STRING "Use lld as the shared linker" FORCE)
+set(CMAKE_MODULE_LINKER_FLAGS "-fuse-ld=lld" CACHE STRING "Use lld as the module linker" FORCE)
+
+# Enable fuzzing
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fsanitize=fuzzer,address,fuzzer-no-link -g")
