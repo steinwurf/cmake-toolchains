e93778bd1ba61a1d6b940f1c1a509ec4e9d3867d
suhrm <suhrminator@gmail.com>
2025-06-19 09:48:19 +0200

    add clang + libc++ toolchain file, some os's still use gnu headers even if clang is specified

---
+++ clang-toolchain-libc++.cmake
@@ -0,0 +1,11 @@
+# clang-toolchain.cmake
+
+# Set the C and C++ compilers to clang
+set(CMAKE_C_COMPILER clang CACHE STRING "C compiler" FORCE)
+set(CMAKE_CXX_COMPILER clang++ CACHE STRING "C++ compiler" FORCE)
+
+# Use LLVM's lld linker
+set(CMAKE_EXE_LINKER_FLAGS "-fuse-ld=lld" CACHE STRING "Use lld as the linker" FORCE)
+set(CMAKE_SHARED_LINKER_FLAGS "-fuse-ld=lld" CACHE STRING "Use lld as the shared linker" FORCE)
+set(CMAKE_MODULE_LINKER_FLAGS "-fuse-ld=lld" CACHE STRING "Use lld as the module linker" FORCE)
+set(CMAKE_CXX_FLAGS "-stdlib=libc++" CACHE STRING "Use libc++ for C++ standard library" FORCE)
