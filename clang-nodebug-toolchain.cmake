3bb4ce219364ec3cad9f7eea0e1eb8d5b3415640
Jeppe Axelsen <73275529+jaxels20@users.noreply.github.com>
2025-08-21 11:37:06 -0100

    remove llvm as the linker in clang toolchains (#2)
    The deleted code assumed clang to be installed using llvm, however this is not the case, therefore this has been removed

--- clang-nodebug-toolchain.cmake
+++ clang-nodebug-toolchain.cmake
@@ -4,11 +4,6 @@
 set(CMAKE_C_COMPILER clang CACHE STRING "C compiler" FORCE)
 set(CMAKE_CXX_COMPILER clang++ CACHE STRING "C++ compiler" FORCE)
 
-# Use LLVM's lld linker
-set(CMAKE_EXE_LINKER_FLAGS "-fuse-ld=lld" CACHE STRING "Use lld as the linker" FORCE)
-set(CMAKE_SHARED_LINKER_FLAGS "-fuse-ld=lld" CACHE STRING "Use lld as the shared linker" FORCE)
-set(CMAKE_MODULE_LINKER_FLAGS "-fuse-ld=lld" CACHE STRING "Use lld as the module linker" FORCE)
-
 # Turn "unused variable" into an error (GCC/Clang flags)
 # Use *_INIT so CMake seeds the normal flags when the project configures.
 set(CMAKE_CXX_FLAGS_INIT "${CMAKE_CXX_FLAGS_INIT} -Wunused-variable -Werror=unused-variable")
--- clang-toolchain.cmake
+++ clang-toolchain.cmake
@@ -4,7 +4,3 @@
 set(CMAKE_C_COMPILER clang CACHE STRING "C compiler" FORCE)
 set(CMAKE_CXX_COMPILER clang++ CACHE STRING "C++ compiler" FORCE)
 
-# Use LLVM's lld linker
-set(CMAKE_EXE_LINKER_FLAGS "-fuse-ld=lld" CACHE STRING "Use lld as the linker" FORCE)
-set(CMAKE_SHARED_LINKER_FLAGS "-fuse-ld=lld" CACHE STRING "Use lld as the shared linker" FORCE)
-set(CMAKE_MODULE_LINKER_FLAGS "-fuse-ld=lld" CACHE STRING "Use lld as the module linker" FORCE)
